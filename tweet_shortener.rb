73b73677360097257f22ef22955294039a29df99
Jerold Kim <iamjooyoung@gmail.com>
2018-11-12 15:38:41 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -54,7 +54,7 @@ def selective_tweet_shortener(tweet)
 end
 
 def shortened_tweet_truncator(tweet)
-  tweet.select(" ").map do |phrase|
+  tweet[0..139]
     if phrase.length > 140
       word_substituter(phrase)[0..140] + "..."
     elsif phrase.length <= 140
